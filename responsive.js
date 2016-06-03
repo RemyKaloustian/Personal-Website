adf682b6e5310510ce6f18cbab1f7714b715bca3
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-03 20:03:27 +0200

    fix #2 fix #3

---
+++ responsive.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Rémy Kaloustian on 03/06/2016.
+ */
+
+
+$(document).ready(function()
+{
+
+    if($(document).width() > 1381)
+    {
+       /* var sidebar_width = $("#sidebar").width();
+
+        console.log("sidebar width : " + sidebar_width);
+
+        var document_width = $(document).width();
+
+        console.log("document width : "  + document_width);
+
+        var project_width = (document_width - sidebar_width)/2  - 30;
+        console.log("project width : " + project_width);
+
+
+
+        $(".projects").each(function()
+        {
+            $(this).css("width",project_width + "px");
+        });*/
+    }
+
+});
