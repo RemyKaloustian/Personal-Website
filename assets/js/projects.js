5224044b728bd19d434761545fbc9982d6a451ec
Rémy Kaloustian <remy.kaloustian@gmail.com>
2016-06-13 07:38:02 +0200

    fix #28, fix #27 :clap:

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -14,11 +14,28 @@ $(document).ready(function(){
         $("#loading_popup").css("display", "block");
         $("#details_popup").css("display", "none");
 
+        $("#project_title").text($(this).data("title"));
+        $("#project_role").text($(this).data("role"));
+        $("#project_skills").text($(this).data("skills"));
+        $("#project_review").text($(this).data("review"));
+
+        if($(this).data("more").length >0)
+        {
+            $("#project_link").text($(this).data("more"));
+            $("#project_link").attr("href", $(this).data("link"));
+
+        }
+
+
         setTimeout(function(){
             $("#loading_popup").css("display", "none");
            $("#details_popup").css("display", "block");
 
         }, 2000);
+
+
+
+
     });
 
 
